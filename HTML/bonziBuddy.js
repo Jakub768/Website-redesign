a03b74771e8eabe9f97add84f2e53784b0f5a06c
Kubsy <kubadd475@gmail.com>
2023-02-16 14:47:50 +0000

    make dynamic scaling work with on mouse events

--- HTML/bonziBuddy.js
+++ HTML/bonziBuddy.js
@@ -1,4 +1,5 @@
-var textSize = "30px";
+var textSize = "25px";
+var textPrevSize = "17px";
 
 function changeSize()
 {
@@ -6,3 +7,8 @@ function changeSize()
     element.style.fontSize = textSize;
 }
 
+function changePrevSize()
+{
+    var element = document.getElementById("link");
+    element.style.fontSize = textPrevSize;
+}
