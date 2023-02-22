6f04a6e85b27d63014b30079ceff2f8dbd87de4e
Jakub <kubabilinski03@gmail.com>
2023-02-22 20:27:38 +0000

    alert on submit button

--- HTML/bonziBuddy.js
+++ HTML/bonziBuddy.js
@@ -1,1 +1,7 @@
-var textSize = 20;
+function buttonOn()
+{
+    if (document.getElementById('name').value == '' || document.getElementById('email').value == '' || document.getElementById('message').value == '')
+        alert("one of the fields are empty! Make sure to fill all of them")
+    else 
+        alert("Message submitted, thank you!")
+}
