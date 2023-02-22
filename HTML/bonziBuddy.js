b047088a1f330aca28a8cb4b9c62c950d7f6ee3d
HamishMAD <hamishmdawson@gmail.com>
2023-02-22 19:25:05 +0000

    Smoother and nicer nav bar experince

--- HTML/bonziBuddy.js
+++ HTML/bonziBuddy.js
@@ -1,14 +1,1 @@
-var textSize = "25px";
-var textPrevSize = "17px";
-
-function changeSize()
-{
-    var element = document.getElementById("link");
-    element.style.fontSize = textSize;
-}
-
-function changePrevSize()
-{
-    var element = document.getElementById("link");
-    element.style.fontSize = textPrevSize;
-}
+var textSize = 20;
