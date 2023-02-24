32f2e50e7d0465ef0fcf3e7ce392d73675704beb
Kubsy <kubadd475@gmail.com>
2023-02-24 19:17:56 +0000

    format if statement, add function to clear fields.

--- HTML/bonziBuddy.js
+++ HTML/bonziBuddy.js
@@ -1,7 +1,18 @@
 function buttonOn()
 {
-    if (document.getElementById('name').value == '' || document.getElementById('email').value == '' || document.getElementById('message').value == '')
+    if (document.getElementById('name').value == '' ||
+        document.getElementById('email').value == '' ||
+        document.getElementById('message').value == '')
         alert("one of the fields are empty! Make sure to fill all of them")
-    else 
+    else {
         alert("Message submitted, thank you!")
+        clearfields()
+    }
+}
+
+function clearfields()
+{
+    document.getElementById('name').value = ''
+    document.getElementById('email').value = ''
+    document.getElementById('message').value = ''
 }
