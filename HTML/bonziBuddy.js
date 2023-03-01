61428baed82f633c7af26bdfa96302e1e0c103fc
Kubsy <kubadd475@gmail.com>
2023-03-01 22:40:23 +0000

    update

--- HTML/bonziBuddy.js
+++ HTML/bonziBuddy.js
@@ -20,12 +20,10 @@ function clearfields()
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
